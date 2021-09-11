b04431aa5945adaaddc8776b7536c5faa7e6bf19
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2021-09-11 11:31:27 +0100

    fix: remove commented code and improve logo

--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -17,9 +17,7 @@
           shrink
           class="row items-center no-wrap"
         >
-          <img
-            src="https://cdn.quasar.dev/img/layout-gallery/logo-google.svg"
-          />
+          <q-btn round unelevated color="primary" icon="person" />
           <span class="q-ml-sm">Contacts</span>
         </q-toolbar-title>
 
@@ -169,83 +167,6 @@
 
     <q-page-container class="GPL__page-container">
       <router-view />
-
-      <!-- <q-page-sticky v-if="$q.screen.gt.sm" expand position="left">
-        <div class="fit q-pt-xl q-px-sm column">
-          <q-btn
-            round
-            flat
-            color="grey-8"
-            stack
-            no-caps
-            size="26px"
-            class="GPL__side-btn"
-          >
-            <q-icon size="22px" name="photo" />
-            <div class="GPL__side-btn__label">Photos</div>
-          </q-btn>
-
-          <q-btn
-            round
-            flat
-            color="grey-8"
-            stack
-            no-caps
-            size="26px"
-            class="GPL__side-btn"
-          >
-            <q-icon size="22px" name="collections_bookmark" />
-            <div class="GPL__side-btn__label">Albums</div>
-          </q-btn>
-
-          <q-btn
-            round
-            flat
-            color="grey-8"
-            stack
-            no-caps
-            size="26px"
-            class="GPL__side-btn"
-          >
-            <q-icon size="22px" name="assistant" />
-            <div class="GPL__side-btn__label">Assistant</div>
-            <q-badge
-              floating
-              color="red"
-              text-color="white"
-              style="top: 8px; right: 16px"
-            >
-              1
-            </q-badge>
-          </q-btn>
-
-          <q-btn
-            round
-            flat
-            color="grey-8"
-            stack
-            no-caps
-            size="26px"
-            class="GPL__side-btn"
-          >
-            <q-icon size="22px" name="group" />
-            <div class="GPL__side-btn__label">Sharing</div>
-          </q-btn>
-
-          <q-btn
-            round
-            flat
-            color="grey-8"
-            stack
-            no-caps
-            size="26px"
-            class="GPL__side-btn"
-          >
-            <q-icon size="22px" name="import_contacts" />
-            <div class="GPL__side-btn__label">Photo books</div>
-          </q-btn>
-        </div>
-      </q-page-sticky> -->
     </q-page-container>
   </q-layout>
 </template>
@@ -254,7 +175,7 @@
 import { ref } from "vue";
 
 export default {
-  name: "GooglePhotosLayout",
+  name: "GoogleContactsLayout",
 
   setup() {
     const leftDrawerOpen = ref(false);
