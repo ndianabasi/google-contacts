b5fba340517dc2e7460d4072b33a4d9910424c24
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2021-09-28 19:04:49 +0100

    feat(api): setup `lucid` and `auth` packages

---
+++ api/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['plugin:adonis/typescriptApp', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+}
